4760123fa0d456de6e716555b1f045289bd996a0
harsharay <harsharay.ray@gmail.com>
2021-01-20 23:04:33 +0530

    Ready to dispatch ;)

--- src/Components/GraphComponent/GraphComponent.js
+++ src/Components/GraphComponent/GraphComponent.js
@@ -1,8 +1,158 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import DonutChart from 'react-donut-chart';
+import { createStockDocument , firestore } from "../../FirebaseFunctions/firebase.utils";
 
-const GraphComponent = () => {
+import "./GraphComponent.css"
+
+const GraphComponent = ({ stockName }) => {
+
+    const [userChoiceFromStorage, setUserChoiceFromStorage] = useState({})
+    const [buttonClicked, setButtonClicked] = useState(false)
+    const [buyValue, setBuyValue] = useState(0)
+    const [sellValue, setSellValue] = useState(0)
+    const [holdValue, setHoldValue] = useState(0)
+
+    useEffect(() => {
+        createStockDocument(stockName)
+    },[])
+
+    const getPercentDetailsFirestore = async () => {
+        let reference = await firestore.collection('stockDetails').where('stockName','==',stockName).get()
+        reference.forEach(item => {
+            setBuyValue(item.data().buyPercent)
+            setSellValue(item.data().sellPercent)
+            setHoldValue(item.data().holdPercent)
+        })
+
+        //Retrieving the chart data on every snapshot change
+        firestore.collection('stockDetails').where('stockName','==', stockName).onSnapshot(querySnapshot => {
+            querySnapshot.docChanges().forEach(change => {
+                setBuyValue(change.doc.data().buyPercent)
+                setSellValue(change.doc.data().sellPercent)
+                setHoldValue(change.doc.data().holdPercent)
+              });
+        })
+        
+    }
+
+    //This is to update the page on the page load
+    useEffect(() => {
+        getPercentDetailsFirestore()
+    },[])
+
+
+    const handleBuySellHoldPercent = async e => {
+        let buttonValue = e.target.value
+        
+        if(buttonValue === "Buy") {
+            setButtonClicked(true)
+            setBuyValue(buyValue + 1)
+            localStorage.setItem(`${stockName}`,JSON.stringify({stockName: stockName , action: 'BUY'}))
+            await firestore.doc(`/stockDetails/${stockName}`).update({
+                buyPercent : buyValue + 1
+            })
+            setButtonClicked(false)
+        } else if(buttonValue === "Sell") {
+            setButtonClicked(true)
+            setSellValue(sellValue + 1)
+            localStorage.setItem(`${stockName}`,JSON.stringify({stockName: stockName , action: 'SELL'}))
+            await firestore.doc(`/stockDetails/${stockName}`).update({
+                sellPercent : sellValue + 1
+            })
+            setButtonClicked(false)
+        } else {
+            setButtonClicked(true)
+            setHoldValue(holdValue + 1)
+            localStorage.setItem(`${stockName}`,JSON.stringify({stockName: stockName , action: 'HOLD'}))
+            await firestore.doc(`/stockDetails/${stockName}`).update({
+                holdPercent : holdValue + 1
+            })
+            setButtonClicked(false)
+        }
+    }
+
+    //Removing LocalStorage after 15 mins
+    useEffect(() => {
+        if(localStorage.getItem(`${stockName}`)) {
+            setTimeout(() => {
+                localStorage.removeItem(`${stockName}`)
+            }, 900000)  // set for 15 minutes currently
+        }
+        
+    },[buttonClicked])
+
+    //GETTING USER CHOICE FROM STORAGE
+    const getUserChoiceFromStorage = () => {
+        let storageData = JSON.parse(localStorage.getItem(`${stockName}`))
+        setUserChoiceFromStorage(storageData)  // SETUSERCHOICE IS A STATE CONTAINER; GETUSERCHOICE IS A FUNCTION
+    }
+
+    useEffect(() => {
+        let storageData = localStorage.getItem(`${stockName}`)
+        if(!storageData) {
+            setUserChoiceFromStorage({})
+        } else {
+            setUserChoiceFromStorage(JSON.parse(storageData))
+        }
+    },[])
+
+    useEffect(() => {
+        if(buttonClicked) {
+            getUserChoiceFromStorage()
+        }
+    },[buttonClicked])
+ 
+    const totalValue = buyValue + sellValue + holdValue
+
+    const buyPercent = buyValue > 0 ? parseFloat(((buyValue/totalValue)* 100).toFixed(2)) :  0
+    const sellPercent = sellValue > 0 ? parseFloat(((sellValue/totalValue)* 100).toFixed(2)) : 0
+    const holdPercent = holdValue > 0 ? parseFloat(((holdValue/totalValue)* 100).toFixed(2)) : 0
+
+
     return (
-        <h1>test</h1>
+        <div>
+                    {/* Donut chart */}
+                    <DonutChart 
+                        data = {[
+                            {
+                                label: 'Buy',
+                                value: buyPercent
+                            },
+                            {
+                                label: 'Sell',
+                                value: sellPercent
+                            },
+                            {
+                                label: 'Hold',
+                                value: holdPercent
+                            }
+                        ]}
+                        colors = {['#2a9d8f' , '#e76f51', '#e9c46a']}
+                        onMouseEnter = {() => false}
+                    />
+                    <div className="buttonBlock"> 
+                        {/* Condition for buy button to display */}
+                        { (userChoiceFromStorage.stockName !== stockName 
+                            || 
+                        ((userChoiceFromStorage.action !== "BUY" ) && (userChoiceFromStorage.stockName === stockName || !userChoiceFromStorage.stockName ))) 
+                        && 
+                        <button onClick={handleBuySellHoldPercent} value="Buy" className="buyButton">Buy</button>} 
+
+                        {/* Condition for sell button to display */}
+                        { (userChoiceFromStorage.stockName !== stockName 
+                            || 
+                        ((userChoiceFromStorage.action !== "SELL") && (userChoiceFromStorage.stockName === stockName || !userChoiceFromStorage.stockName ))) 
+                        && 
+                        <button onClick={handleBuySellHoldPercent} value="Sell" className="sellButton">Sell</button>}
+
+                        {/* Condition for hold button to display */}
+                        { (userChoiceFromStorage.stockName !== stockName 
+                            || 
+                        ((userChoiceFromStorage.action !== "HOLD") && (userChoiceFromStorage.stockName === stockName || !userChoiceFromStorage.stockName ))) 
+                        && 
+                        <button onClick={handleBuySellHoldPercent} value="Hold" className="holdButton">Hold</button>}                    
+                    </div>
+                </div>
     )
 }
 
--- src/Components/StockDetailPage/StockDetailsPage.js
+++ src/Components/StockDetailPage/StockDetailsPage.js
@@ -1,167 +1,17 @@
-import React, { useState, useEffect } from "react";
-import DonutChart from 'react-donut-chart';
-import { createStockDocument , firestore } from "../../FirebaseFunctions/firebase.utils";
+import React from "react";
 import ChatComponent from "../ChatComponent/ChatComponent"
+import GraphComponent from "../GraphComponent/GraphComponent";
 
 import "./StockDetailPage.css"
 
 const StockDetailPage = (props) => {
 
-    const [userChoiceFromStorage, setUserChoiceFromStorage] = useState({})
-    const [buttonClicked, setButtonClicked] = useState(false)
-    const [buyValue, setBuyValue] = useState(0)
-    const [sellValue, setSellValue] = useState(0)
-    const [holdValue, setHoldValue] = useState(0)
-
-    useEffect(() => {
-        createStockDocument(props.match.params.stockName)
-    },[])
-
-    const getPercentDetailsFirestore = async () => {
-        let reference = await firestore.collection('stockDetails').where('stockName','==',props.match.params.stockName).get()
-        reference.forEach(item => {
-            setBuyValue(item.data().buyPercent)
-            setSellValue(item.data().sellPercent)
-            setHoldValue(item.data().holdPercent)
-        })
-
-        //Retrieving the chart data on every snapshot change
-        firestore.collection('stockDetails').where('stockName','==', props.match.params.stockName).onSnapshot(querySnapshot => {
-            querySnapshot.docChanges().forEach(change => {
-                // setAllMessages(change.doc.data().chatMessages)
-                setBuyValue(change.doc.data().buyPercent)
-                setSellValue(change.doc.data().sellPercent)
-                setHoldValue(change.doc.data().holdPercent)
-              });
-        })
-        
-    }
-
-    //This is to update the page on the page load
-    useEffect(() => {
-        getPercentDetailsFirestore()
-    },[])
-
-
-    const handleBuySellHoldPercent = async e => {
-        let buttonValue = e.target.value
-        
-        if(buttonValue === "Buy") {
-            setButtonClicked(true)
-            setBuyValue(buyValue + 1)
-            localStorage.setItem(`${props.match.params.stockName}`,JSON.stringify({stockName: props.match.params.stockName , action: 'BUY'}))
-            await firestore.doc(`/stockDetails/${props.match.params.stockName}`).update({
-                buyPercent : buyValue + 1
-            })
-            setButtonClicked(false)
-        } else if(buttonValue === "Sell") {
-            setButtonClicked(true)
-            setSellValue(sellValue + 1)
-            localStorage.setItem(`${props.match.params.stockName}`,JSON.stringify({stockName: props.match.params.stockName , action: 'SELL'}))
-            await firestore.doc(`/stockDetails/${props.match.params.stockName}`).update({
-                sellPercent : sellValue + 1
-            })
-            setButtonClicked(false)
-        } else {
-            setButtonClicked(true)
-            setHoldValue(holdValue + 1)
-            localStorage.setItem(`${props.match.params.stockName}`,JSON.stringify({stockName: props.match.params.stockName , action: 'HOLD'}))
-            await firestore.doc(`/stockDetails/${props.match.params.stockName}`).update({
-                holdPercent : holdValue + 1
-            })
-            setButtonClicked(false)
-        }
-    }
-
-    //Removing LocalStorage after 15 mins
-    useEffect(() => {
-        if(localStorage.getItem(`${props.match.params.stockName}`)) {
-            setTimeout(() => {
-                localStorage.removeItem(`${props.match.params.stockName}`)
-            }, 900000)  // set for 15 minutes currently
-        }
-        
-    },[buttonClicked])
-
-    //GETTING USER CHOICE FROM STORAGE
-    const getUserChoiceFromStorage = () => {
-        let storageData = JSON.parse(localStorage.getItem(`${props.match.params.stockName}`))
-        setUserChoiceFromStorage(storageData)  // SETUSERCHOICE IS A STATE CONTAINER; GETUSERCHOICE IS A FUNCTION
-    }
-
-    useEffect(() => {
-        let storageData = localStorage.getItem(`${props.match.params.stockName}`)
-        if(!storageData) {
-            setUserChoiceFromStorage({})
-        } else {
-            setUserChoiceFromStorage(JSON.parse(storageData))
-        }
-    },[])
-
-    useEffect(() => {
-        if(buttonClicked) {
-            getUserChoiceFromStorage()
-        }
-    },[buttonClicked])
- 
-    const totalValue = buyValue + sellValue + holdValue
-
-    const buyPercent = buyValue > 0 ? parseFloat(((buyValue/totalValue)* 100).toFixed(2)) :  0
-    const sellPercent = sellValue > 0 ? parseFloat(((sellValue/totalValue)* 100).toFixed(2)) : 0
-    const holdPercent = holdValue > 0 ? parseFloat(((holdValue/totalValue)* 100).toFixed(2)) : 0
-
-
     return (
         <>
             <h1>{props.match.params.stockName}</h1>
             <div className="stockDetailPage-Content">
-                <div>
-                    {/* Donut chart */}
-                    <DonutChart 
-                        data = {[
-                            {
-                                label: 'Buy',
-                                value: buyPercent
-                            },
-                            {
-                                label: 'Sell',
-                                value: sellPercent
-                            },
-                            {
-                                label: 'Hold',
-                                value: holdPercent
-                            }
-                        ]}
-                        colors = {['#2a9d8f' , '#e76f51', '#e9c46a']}
-                        onMouseEnter = {() => false}
-                    />
-                    <div className="buttonBlock"> 
-                        {/* Condition for buy button to display */}
-                        { (userChoiceFromStorage.stockName !== props.match.params.stockName 
-                        || 
-                        ((userChoiceFromStorage.action !== "BUY" ) && (userChoiceFromStorage.stockName === props.match.params.stockName || !userChoiceFromStorage.stockName ))) 
-                        && 
-                        <button onClick={handleBuySellHoldPercent} value="Buy" className="buyButton">Buy</button>} 
-
-                        {/* Condition for sell button to display */}
-                        { (userChoiceFromStorage.stockName !== props.match.params.stockName 
-                        || 
-                        ((userChoiceFromStorage.action !== "SELL") && (userChoiceFromStorage.stockName === props.match.params.stockName || !userChoiceFromStorage.stockName ))) 
-                        && 
-                        <button onClick={handleBuySellHoldPercent} value="Sell" className="sellButton">Sell</button>}
-
-                        {/* Condition for hold button to display */}
-                        { (userChoiceFromStorage.stockName !== props.match.params.stockName 
-                        || 
-                        ((userChoiceFromStorage.action !== "HOLD") && (userChoiceFromStorage.stockName === props.match.params.stockName || !userChoiceFromStorage.stockName ))) 
-                        && 
-                        <button onClick={handleBuySellHoldPercent} value="Hold" className="holdButton">Hold</button>}                    
-                    </div>
-                </div>
-                <div className="chatBlock">
-                    {/* Chat block */}
-                    <ChatComponent stockName={props.match.params.stockName}/>
-                </div>
+                <GraphComponent stockName={props.match.params.stockName}/>
+                <ChatComponent stockName={props.match.params.stockName}/>
             </div>
         </>
     )
